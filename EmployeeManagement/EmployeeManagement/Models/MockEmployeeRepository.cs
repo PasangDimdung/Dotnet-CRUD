7b200e644965f58df6dc774fc4543ac236c1471b
PasangDimdung <pasangdimdung045@gmail.com>
2021-02-01 20:15:57 +0545

    ïnitial commit

---
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    /*Reason for using IEmployeeRepository interface is: Without it we cannot use Dependency Injection as a result 
+    our application will be extremely difficult to change and maintain. Unit Testing is also becomes difficult.*/
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id= 1, Name = "Hari", Email = "hari@gmail.com", Department = "IT"},
+                new Employee() { Id= 2, Name = "Ram", Email = "ram@gmail.com", Department = "HR" },
+                new Employee() { Id= 3, Name = "Shyam", Email = "shyam@gmail.com", Department = "IT" }
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            /* If someone requests the IEmployeeRepository then it creates an instance of MockEmployeeRepository 
+            and injects that instance */
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
