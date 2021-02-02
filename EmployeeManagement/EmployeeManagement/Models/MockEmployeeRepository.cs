447509a7e9c7e704665cc27dc6d76e483e89280e
PasangDimdung <pasangdimdung045@gmail.com>
2021-02-02 11:17:07 +0545

    added layout

--- EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Models
 {
-    /*Reason for using IEmployeeRepository interface is: Without it we cannot use Dependency Injection as a result 
-    our application will be extremely difficult to change and maintain. Unit Testing is also becomes difficult.*/
+    /* Reason for using IEmployeeRepository interface is: Without it we cannot use Dependency Injection as a result 
+    our application will be extremely difficult to change and maintain. Unit Testing is also becomes difficult. */
     public class MockEmployeeRepository : IEmployeeRepository
     {
         private List<Employee> _employeeList;
@@ -16,8 +16,14 @@ namespace EmployeeManagement.Models
             {
                 new Employee() { Id= 1, Name = "Hari", Email = "hari@gmail.com", Department = "IT"},
                 new Employee() { Id= 2, Name = "Ram", Email = "ram@gmail.com", Department = "HR" },
-                new Employee() { Id= 3, Name = "Shyam", Email = "shyam@gmail.com", Department = "IT" }
+                new Employee() { Id= 3, Name = "Shyam", Email = "shyam@gmail.com", Department = "IT" },
+                new Employee() { Id= 4, Name = "Test", Email = "test@gmail.com", Department = "IT" }
             };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
         }
 
         public Employee GetEmployee(int Id)
